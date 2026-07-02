8cf50e49a2ea7ecbd89ea26c302bde016934076e
Raju-ahamed <rajuahamed7320@gmail.com>
2026-07-02 14:23:17 +0600

    2nd repo

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,32 @@ function App() {
       .then(data => useServer(data))
   }, [])
 
+
+  const handlefrom = e => {
+    e.preventDefault();
+    const from = e.target;
+    const name = from.name.value;
+    const email = from.email.value;
+    const user = { name, email }
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        'constent-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+  }
   return (
     <>
       <h1>My Client server </h1>
       <p>Number of user  :{server.length}</p>
+      <form onSubmit={handlefrom}>
+        <input type="text" name="name" id="" /><br />
+        <input type="text" name="email" id="" /><br />
+        <button>submit</button>
+      </form>
+      <div>{server.map(user => <p key={user.id}>{user.name}</p>)}</div>
     </>
   )
 }
